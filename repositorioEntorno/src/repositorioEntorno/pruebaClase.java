2a42c4d442d414a00c93584d0a5f4e596c01e5e5
DAM1 <DAM1@PC>
2025-02-19 08:34:10 +0100

    Commit inicial

---
+++ repositorioEntorno/src/repositorioEntorno/pruebaClase.java
@@ -0,0 +1,11 @@
+package repositorioEntorno;
+
+public class pruebaClase {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo, entorno es mi asignatura favorita");
+
+	}
+
+}
